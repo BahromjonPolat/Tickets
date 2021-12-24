63f188b39503c302db57d27d193ff77343ef48d9
BahromjonPolat <itjunior.uz@gmail.com>
2021-12-24 16:39:17 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/uz/mirrikh/ticket/MainActivity.kt
@@ -0,0 +1,6 @@
+package uz.mirrikh.ticket
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:ticket/view/widgets/home/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ticket',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
