58154b4e6170995f9882130534e8197082fb1c6c
BahromjonPolat <itjunior.uz@gmail.com>
2021-12-24 18:27:48 +0500

    Iconlar AssetIcons class da static konstantalarga yozib qo'yildi

---
+++ lib/core/constants/asset_icons.dart
@@ -0,0 +1,57 @@
+class AssetIcons {
+  // Bottom navigation bar icons
+  static const String homeActive = 'assets/icons/BottomBar/HomeActive.svg';
+  static const String homeInactive = 'assets/icons/BottomBar/HomeInactive.svg';
+  static const String searchActive = 'assets/icons/BottomBar/SearchActive.svg';
+  static const String searchInactive = 'assets/icons/BottomBar/SearchInactive.svg';
+  static const String ticketInactive = 'assets/icons/BottomBar/TicketInactive.svg';
+  static const String ticketActive = 'assets/icons/BottomBar/TicketActive.svg';
+  static const String userActive = 'assets/icons/BottomBar/UserActive.svg';
+  static const String userInactive = 'assets/icons/BottomBar/UserInactive.svg';
+
+  // Button icons
+  static const String calendar = 'assets/icons/Buttons/Calendar.svg';
+  static const String erase = 'assets/icons/Buttons/Erase.svg';
+  static const String filters = 'assets/icons/Buttons/Filters.svg';
+  static const String location = 'assets/icons/Buttons/Location.svg';
+  static const String notify = 'assets/icons/Buttons/Notify.svg';
+  static const String right = 'assets/icons/Buttons/Right.svg';
+  static const String sort = 'assets/icons/Buttons/Sort.svg';
+
+  // Map Pins icons
+  static const String music = 'assets/icons/MapPins/Music.svg';
+  static const String sports = 'assets/icons/MapPins/Sports.svg';
+
+  // Misc icons
+  static const String cancel = 'assets/icons/Misc/Cancel.svg';
+  static const String chevronBottom = 'assets/icons/Misc/Chevron8px.svg';
+  static const String chevronRight= 'assets/icons/Misc/Chevron8px-1.svg';
+  static const String dislike = 'assets/icons/Misc/Dislike.svg';
+  static const String handle = 'assets/icons/Misc/Handle.svg';
+  static const String locationMisc = 'assets/icons/Misc/Location.svg';
+  static const String navigation = 'assets/icons/Misc/Location-1.svg';
+  static const String mapPin = 'assets/icons/Misc/MapPin.svg';
+
+  // Property info icons
+  static const String propertyType = 'assets/icons/PropertyInfo/PropertyType.svg';
+
+  // Tag icons
+  static const String date = 'assets/icons/Tags/Date.svg';
+  static const String musicTag = 'assets/icons/Tags/Music.svg';
+  static const String organizer = 'assets/icons/Tags/Organizer.svg';
+  static const String sport = 'assets/icons/Tags/Sport.svg';
+  static const String ticket = 'assets/icons/Tags/Ticket.svg';
+  static const String venue = 'assets/icons/Tags/Venue.svg';
+
+  // Ticket icons
+  static const String green = 'assets/icons/Tickets/green.svg';
+  static const String blue = 'assets/icons/Tickets/Blue.svg';
+  static const String red = 'assets/icons/Tickets/Red.svg';
+
+  // Top Bar icons
+  static const String black = 'assets/icons/TopBar/Back.svg';
+  static const String like = 'assets/icons/TopBar/Like.svg';
+  static const String placeholder = 'assets/icons/TopBar/Placeholder.svg';
+  static const String search = 'assets/icons/TopBar/Search.svg';
+  static const String share = 'assets/icons/TopBar/Share.svg';
+}
