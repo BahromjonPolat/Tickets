66fe13d38ae9b4e800aecd9d03651014d668fb6d
BahromjonPolat <itjunior.uz@gmail.com>
2021-12-25 19:25:01 +0500

    ProgramInfo vidjeti tayyorlandi

---
+++ lib/view/widgets/components/program_info.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:ticket/core/components/exporting_packages.dart';
+
+class ProgramInfo extends StatelessWidget {
+  List<TextWithIcon> textList;
+  String label;
+  Color color;
+
+  ProgramInfo({
+    required this.label,
+    required this.textList,
+    this.color = ConstColors.white,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        MyText(label, color: color, size: 22.0, weight: FontWeight.w600),
+        SizedBox(height: getProportionateScreenHeight(8.0)),
+        Wrap(children: textList.map((e) => e).toList()),
+      ],
+    );
+  }
+}
