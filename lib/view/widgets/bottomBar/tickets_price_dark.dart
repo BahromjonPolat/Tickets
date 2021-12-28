0756bb9a8f9d9e67fd49504e2e31e42f23cdafb4
BahromjonPolat <itjunior.uz@gmail.com>
2021-12-28 10:46:52 +0500

    BottomBarTicketPriceDark vidjeti tayyor

---
+++ lib/view/widgets/bottomBar/tickets_price_dark.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:ticket/core/components/exporting_packages.dart';
+
+class BottomBarTicketPriceDark extends StatelessWidget {
+  const BottomBarTicketPriceDark({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: getProportionateScreenHeight(91.0),
+      width: getProportionateScreenWidth(375.0),
+      color: ConstColors.darkNavy,
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MyText(
+                  '€30 - €80',
+                  size: 15.0,
+                  weight: FontWeight.w600,
+                  color: ConstColors.white,
+                ),
+                MyText(
+                  'Get now on ebilet.pl',
+                  weight: FontWeight.w700,
+                  size: 10.0,
+                  color: ConstColors.darkerGrey,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: MyEdgeInsets.symmetric(horizontal: 15.0),
+              child: WhiteButton(
+                onPressed: () {},
+                label: 'Buy Tickets',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
