fe0ed0f624f5015c455b55e35350c4bf1c5037a6
BahromjonPolat <itjunior.uz@gmail.com>
2021-12-28 15:45:44 +0500

    TicketsTimeAppBar tayyor

---
+++ lib/view/widgets/topBar/tickets_time_app_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:ticket/core/components/exporting_packages.dart';
+
+class TicketsTimeAppBar extends StatelessWidget with PreferredSizeWidget{
+  const TicketsTimeAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+      systemOverlayStyle: SystemUiOverlayStyle.dark,
+      automaticallyImplyLeading: false,
+      title: MyText('WARSHAW',size: 28, weight: FontWeight.w700),
+      actions: [
+        MyIconButton(onPressed: (){}, assetIcon: AssetIcons.calendar),
+      ],
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size(double.infinity, getProportionateScreenHeight(61.0));
+}
